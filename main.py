67e14e852183bd17b1274af1590cfb29521be67c
INGANZAMARUMPU <jonkur96@gmail.com>
2023-04-09 12:09:54 +0200

    build face 2 and 3

--- main.py
+++ main.py
@@ -65,17 +65,48 @@ def build() -> SpeedCube:
     cube = SpeedCube()
     null = " "*7
     print(f"{null}|FACE 5|{null*2}\n|FACE 4|FACE 1|FACE 2|FACE 3|\n{null}|FACE 6|")
-    print("Saisissez la face 1:")
+    
+    # print("Saisissez la face 1:")
+    # colors = askLine()
+    # cube._11.c1 = colors[0][0]
+    # cube._12.c1 = colors[0][1]
+    # cube._13.c1 = colors[0][2]
+    # cube._14.c1 = colors[1][0]
+    # cube._15.c1 = colors[1][1]
+    # cube._16.c1 = colors[1][2]
+    # cube._17.c1 = colors[2][0]
+    # cube._18.c1 = colors[2][1]
+    # cube._19.c1 = colors[2][2]
+    # print(cube.face(1))
+
+    # print("Saisissez la face 2:")
+    # colors = askLine()
+    # cube._13.c2 = colors[0][2]
+    # cube._16.c2 = colors[0][1]
+    # cube._19.c2 = colors[0][0]
+    # cube._23.c2 = colors[1][2]
+    # cube._26.c2 = colors[1][1]
+    # cube._29.c2 = colors[1][0]
+    # cube._33.c2 = colors[2][2]
+    # cube._36.c2 = colors[2][1]
+    # cube._39.c2 = colors[2][0]
+    # print(cube.face(2))
+
+    print("Saisissez la face 3:")
     colors = askLine()
-    cube._11.c1 = colors[0][0]
-    cube._12.c1 = colors[0][1]
-    cube._13.c1 = colors[0][2]
-    cube._14.c1 = colors[1][0]
-    cube._15.c1 = colors[1][1]
-    cube._16.c1 = colors[1][2]
-    cube._17.c1 = colors[2][0]
-    cube._18.c1 = colors[2][1]
-    cube._19.c1 = colors[2][2]
-    print(cube.face(1))
+    cube._31.c3 = colors[2][0]
+    cube._32.c3 = colors[2][1]
+    cube._33.c3 = colors[2][2]
+    cube._34.c3 = colors[1][0]
+    cube._35.c3 = colors[1][1]
+    cube._36.c3 = colors[1][2]
+    cube._37.c3 = colors[0][0]
+    cube._38.c3 = colors[0][1]
+    cube._39.c3 = colors[0][2]
+    print(cube.face(3))
+
+    cube_2 = SpeedCube()
+    cube_2.move_4to1_1()
+    print(cube_2.face(3))
 
 build()
